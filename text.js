f11966e647bc316c6c292d4f427a336faf2ea38c
AgungDwiSahputra <nezastsanjaya1@gmail.com>
2022-05-22 14:51:39 +0700

    Pertama

---
+++ text.js
@@ -0,0 +1,10 @@
+// Untuk Nav Bagian home
+const text_home = {
+    header:"Barakallah fi umrik Sayang Ku Salma Hanipah", 
+    doa:"Semoga kamu bisa menjadi pribadi yang lebih bertakwa lagi dan semoga kamu selalu diberikan rezeki yang berlimpah serta keselamatan di dunia dan akhirat. Satu harapanku di hari spesialmu ini.. Semoga kita selalu bersama dalam satu cinta yang utuh. Aamiin ya rabbal alamin..."
+};
+// Untuk Nav Bagian galeri
+const text_galeri = {
+    header_galeri:"~ Galeri Kita ~"
+};
+
